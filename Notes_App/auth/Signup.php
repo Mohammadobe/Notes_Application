1837c2a08afc8bb765baeadd334fa67ab92b1df6
Mohammadobe <154047952+Mohammadobe@users.noreply.github.com>
2024-08-13 15:00:13 +0300

    ..

---
+++ Notes_App/auth/Signup.php
@@ -0,0 +1,22 @@
+<?php
+
+    include "../Connect.php";
+
+    $username = filterRequest("username");
+    $email    = filterRequest("email");
+    $password = filterRequest("password");
+
+    $stmt = $connect->prepare("INSERT INTO `users`(`username`, `email`, `password`) 
+                               VALUES (?,?,?)");
+    
+    $stmt->execute(array($username , $email , $password));
+
+    $count = $stmt->rowCount();
+
+    if($count > 0){
+        echo json_encode(array("Status" => "Success"));
+    } else{
+        echo json_encode(array("Status" => "Failed"));
+    }
+
+?>
